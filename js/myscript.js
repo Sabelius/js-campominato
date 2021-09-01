bd5fbc068c5c707dbde6448d91c9e41aae73f908
Sabelius <sbebby92@virgilio.it>
2021-09-01 15:30:57 +0200

    aggiornamento

--- js/myscript.js
+++ js/myscript.js
@@ -5,17 +5,34 @@ let playerNumber = [];
 let points;
 console.log(arrBomb);
 
-function getRandomNumber(min, max) {
-  return (Math.floor(Math.random() * (max - min + 1) + min));
+while (arrBomb.length < 16) {
+  numberBomb = Math.floor(Math.random() * 100) + 1;
+  if (arrBomb.indexOf(numberBomb) === -1) {
+    arrBomb.push(numberBomb);
+  }
 }
-
-
-for (i = 1; i <= 16; i++) {
-  numberBomb = getRandomNumber(1, 100);
-  arrBomb.push(numberBomb);
-}
-
 console.log(arrBomb);
+
+
+function numeriInArray (array, elemento){
+  let i = 0;
+  if(array[i] == elemento){
+    return true;
+  }
+  return false;
+}
+
+
+while (playerNumber.length < (100 - 16)) {
+  questionNumber = parseInt(prompt("Inserisci un numero:"));
+  if(numeriInArray(arrBomb, questionNumber) == false){
+    playerNumber.push(questionNumber);
+  }else if(numeriInArray(arrBomb, questionNumber) == true){
+    console.log("BOOOOOOOOOM!!!");
+  }
+}
+
+
 
 
 
