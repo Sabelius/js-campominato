a6e1706ffd2096531f61b20d7949c6e445384dc9
Sabelius <sbebby92@virgilio.it>
2021-09-02 15:11:11 +0200

    prima bozza esercizio completo

--- js/myscript.js
+++ js/myscript.js
@@ -9,8 +9,9 @@ let explosion = false;
 let playerNumber = [];
 
 level = parseInt(prompt("Inserisci un livello di difficoltà tra 0, 1 e 2:"));
+document.getElementById("difficoltà").innerHTML = level;
 if (level != 0 && level != 1 && level != 2) {
-  level = parseInt(prompt("Inserisci un livello di difficoltà esistente:"));
+  document.getElementById("avviso").innerHTML = "Inserisci un livello di difficoltà esistente!!!";
 }
 
 switch (level) {
@@ -66,19 +67,24 @@ while (playerNumber.length < (maxRangeNumber - minRangeNumber) && explosion == f
     if (numbersInArray(playerNumber, questionNumber) == false && questionNumber >= 1 && questionNumber <= randomMaxNumber) {
       playerNumber.push(questionNumber);
     } else {
-      console.log("Inserisci un numero valido!!!");
+      alert("Inserisci un numero valido!!!");
     }
   } else if (numbersInArray(arrBomb, questionNumber) == true) {
     console.log("BOOOOOOOOOM!!! La partita è terminata!!!");
+    document.getElementById("sconfitta").innerHTML = "BOOOOOOOOM!!! La partita è terminata!!!";
+    document.getElementById("bombe").innerHTML = "Le bombe erano i seguenti numeri: " + arrBomb;
     explosion = true;
   }
   console.log(playerNumber);
+  document.getElementById("numeriscelti").innerHTML = playerNumber;
 }
 
 console.log("Hai realizzato un punteggio di: " + playerNumber.length);
+document.getElementById("punteggio").innerHTML = playerNumber.length;
 
 if (playerNumber.length == (maxRangeNumber - minRangeNumber)) {
   console.log("Hai vinto0o0o0o0o0o0o!!!");
+  document.getElementById("vittoria").innerHTML = "Hai vinto0o0o0oo0o0o0o0o!!!";
 }
 
 
