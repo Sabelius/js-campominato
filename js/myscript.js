5f3179e1b8e5262715abea2967b33e7d750c70f9
Sabelius <sbebby92@virgilio.it>
2021-09-02 11:34:24 +0200

    versione finale con bonus

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,37 @@
+let livello;
+let minRangeNumber;
+let randomMaxNumber;
 let arrBomb = [];
 let numberBomb = 16;
 let questionNumber;
 let explosion = false;
 let playerNumber = [];
 
-function getRandomNumbers(bombLength){
+livello = parseInt(prompt("Inserisci un livello di difficoltà tra 0, 1 e 2:"));
+if (livello != 0 && livello != 1 && livello != 2) {
+  livello = alert("Inserisci un livello di difficoltà esistente!!!");
+}
+
+switch (livello) {
+  case 0:
+    minRangeNumber = 15;
+    randomMaxNumber = 100;
+  break;
+  case 1:
+    minRangeNumber = 14;
+    randomMaxNumber = 80;
+  break;
+  case 2:
+    minRangeNumber = 12;
+    randomMaxNumber = 50;
+  break;
+}
+
+
+function getRandomNumbers(bombLength) {
   let array = [];
   while (array.length < bombLength) {
-    numberBomb = Math.floor(Math.random() * 100) + 1;
+    numberBomb = Math.floor(Math.random() * randomMaxNumber) + 1;
     if (array.indexOf(numberBomb) === -1) {
       array.push(numberBomb);
     }
@@ -20,7 +44,7 @@ console.log("Le bombe sono i seguenti numeri: " + arrBomb);
 
 
 
-function numeriInArray(array, element) {
+function numbersInArray(array, element) {
   let i = 0;
   for (i = 0; i < array.length; i++) {
     if (array[i] == element) {
@@ -32,24 +56,24 @@ function numeriInArray(array, element) {
 
 
 
-while (playerNumber.length < (20 - 16) && explosion == false) {
+while (playerNumber.length < (20 - minRangeNumber) && explosion == false) {
   questionNumber = parseInt(prompt("Inserisci un numero:"));
-  if (numeriInArray(arrBomb, questionNumber) == false) {
-    if(numeriInArray(playerNumber, questionNumber) == false && questionNumber >= 1 && questionNumber <=100){
+  if (numbersInArray(arrBomb, questionNumber) == false) {
+    if (numbersInArray(playerNumber, questionNumber) == false && questionNumber >= 1 && questionNumber <= randomMaxNumber) {
       playerNumber.push(questionNumber);
-    }else{
+    } else {
       console.log("Inserisci un numero valido!!!");
     }
-  } else if (numeriInArray(arrBomb, questionNumber) == true) {
+  } else if (numbersInArray(arrBomb, questionNumber) == true) {
     console.log("BOOOOOOOOOM!!! La partita è terminata!!!");
     explosion = true;
-  } 
+  }
   console.log(playerNumber);
 }
 
 console.log("Hai realizzato un punteggio di: " + playerNumber.length);
 
-if( playerNumber.length == (20 - 16)){
+if (playerNumber.length == (20 - minRangeNumber)) {
   console.log("Hai vinto0o0o0o0o0o0o!!!");
 }
 
